b2688aa6d913c5c417f1b218b2f15ed045145b72
ameo <joshuabbraun@gmail.com>
2016-05-25 20:57:53 +0200

    Grundaufbau des Product-Service

---
+++ Microservice/Product/src/main/java/de/hska/iwi/microservice/product/ProductServer.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright (c) 2016.   Joshua Braun
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package de.hska.iwi.microservice.product;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by ameo on 25.05.16.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProductServer {
+    private static Logger logger = Logger.getLogger(ProductServer.class);
+
+    public static void main(String[] args) {
+        logger.info("Erzeuge Product-Service");
+        System.setProperty("spring.config.name", "Product-Server");
+        SpringApplication.run(ProductServer.class, args);
+    }
+}
