97ac2b4785b50cb63fcb0454251d08a7e2939e97
Joshua Braun <joshuabraun@web.de>
2016-11-11 14:43:24 +0100

    Produkte werden ordnungsgemäß angelegt/gelöscht

--- Microservice/Product/src/main/java/de/hska/iwi/microservice/product/web/ProductController.java
+++ Microservice/Product/src/main/java/de/hska/iwi/microservice/product/web/ProductController.java
@@ -5,11 +5,7 @@ import de.hska.iwi.microservice.product.service.IProductServiceFacade;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,14 +25,14 @@ public class ProductController implements IProductController {
 
     @Override
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Product createProduct(@ModelAttribute Product product) {
+    public Product createProduct(@RequestBody Product product) {
         logger.info("Erzeuge neues Produkt");
         return productServiceFacade.createProduct(product);
     }
 
     @Override
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public Product updateProduct(@PathVariable("id") int id, @ModelAttribute Product product) {
+    public Product updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
         logger.info("Aktualisere vorhandenes Produkt");
         product.setId(id);
         return productServiceFacade.updateProduct(product);
