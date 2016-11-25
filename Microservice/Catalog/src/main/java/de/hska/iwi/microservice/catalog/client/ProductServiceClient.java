075bad0ea026fadb3884a419979f82ce6dd579d2
Joshua Braun <joshuabbraun@gmail.com>
2016-11-25 13:36:33 +0100

    CatalogService greift über RestTemplate auf ProductService zu

--- Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/client/ProductServiceClient.java
+++ Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/client/ProductServiceClient.java
@@ -2,52 +2,51 @@ package de.hska.iwi.microservice.catalog.client;
 
 import de.hska.iwi.microservice.catalog.client.api.ProductService;
 import de.hska.iwi.microservice.catalog.entity.Product;
-import feign.Feign;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
-import feign.jaxrs.JAXRSContract;
 import org.apache.log4j.Logger;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 /**
  * Created by ameo on 13.11.16.
  */
-public class ProductServiceClient implements ProductService{
+public class ProductServiceClient implements ProductService {
     private static final Logger logger = Logger.getLogger(ProductServiceClient.class);
 
-    private final ProductService service;
+    private final String serviceUrl;
+    private final RestTemplate restClient = new RestTemplate();
 
-    public ProductServiceClient(String productServiceUrl) {
-        this.service = Feign.builder()
-                .contract(new JAXRSContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(ProductService.class, productServiceUrl);
-    }
-
-    @Override
-    public Product createProduct(Product product) {
-        return service.createProduct(product);
-    }
-
-    @Override
-    public Product updateProduct(int id, Product product) {
-        return null;
-    }
-
-    @Override
-    public boolean deleteProduct(int id) {
-        return service.deleteProduct(id);
+    public ProductServiceClient(String serviceUrl) {
+        this.serviceUrl = serviceUrl;
     }
 
     @Override
     public List<Product> getProducts(int categoryId) {
-        return service.getProducts(categoryId);
+        return restClient.getForObject(String.format("%s/?categoryId=%d", serviceUrl, categoryId), List.class);
     }
 
     @Override
     public Product getProduct(int id) {
-        return service.getProduct(id);
+        return restClient.getForObject(String.format("%s/%d", serviceUrl, id), Product.class);
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return restClient.postForEntity(String.format("%s/", serviceUrl),
+                                            product,
+                                            Product.class).getBody();
+    }
+
+    @Override
+    public Product updateProduct(int id, Product product) {
+        restClient.put(String.format("%s/%d", serviceUrl, id),
+                                product);
+        return this.getProduct(id);
+    }
+
+    @Override
+    public boolean deleteProduct(int id) {
+        restClient.delete(String.format("%s/%d", serviceUrl, id));
+        return !(this.getProduct(id) instanceof Product);
     }
 }
