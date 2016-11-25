400925e52a9c83079bd42f412dcc2783488d0071
Joshua Braun <joshuabbraun@gmail.com>
2016-11-25 13:38:06 +0100

    Anpassung an Service und Fassade um dynamisch Clients einzubinden

--- Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/CatalogServer.java
+++ Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/CatalogServer.java
@@ -1,9 +1,11 @@
 package de.hska.iwi.microservice.catalog;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * Created by ameo on 13.11.16.
