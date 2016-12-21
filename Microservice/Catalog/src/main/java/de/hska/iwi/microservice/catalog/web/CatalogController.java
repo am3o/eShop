fccc7b802a027a8f9ecb53325ebac9d79328f68c
Joshua Braun <joshuabbraun@gmail.com>
2016-12-21 12:44:01 +0100

    Integration des Loging-Mechanismus

--- Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/web/CatalogController.java
+++ Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/web/CatalogController.java
@@ -30,84 +30,108 @@ public class CatalogController implements ICatalogController {
     @Override
     @RequestMapping(value = "/category", method = RequestMethod.GET)
     public ResponseEntity<List<Category>> getAllCategories() {
+        logger.info("Liefere alle Kategorien zurück.");
         return new ResponseEntity<List<Category>>(serviceFacade.getAllCategories(), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/category", method = RequestMethod.POST)
     public ResponseEntity<Category> createCategory(@RequestHeader("usr") String username, @RequestHeader("pass") String password, @RequestBody Category category) {
-        if(!serviceFacade.checkPermission(new Credential(username, password)))
+        logger.info("Erzeuge neue Kategorie im Sortiment.");
+        if(!serviceFacade.checkPermission(new Credential(username, password))){
+            logger.info("Keine gültigen Benutzerrechte liegen vor.");
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Category>(serviceFacade.createCategory(category), HttpStatus.CREATED);
     }
 
     @Override
     @RequestMapping(value = "/category/{categoryId}", method = RequestMethod.PUT)
     public ResponseEntity<Category> updateCategory(@RequestHeader("usr") String username, @RequestHeader("pass") String password, @PathVariable(value = "categoryId") int categoryId, @RequestBody Category category) {
-        if(!serviceFacade.checkPermission(new Credential(username, password)))
+        logger.info("Aktualisiere vorhandene Kategorie.");
+        if(!serviceFacade.checkPermission(new Credential(username, password))){
+            logger.info("Keine gültigen Benutzerrechte liegen vor.");
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Category>(serviceFacade.updateCategory(categoryId, category), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/category/{categoryId}", method = RequestMethod.GET)
     public ResponseEntity<Category> getCategory(@PathVariable(value = "categoryId") int categoryId) {
+        logger.info(String.format("Liefere spezielle Kategorie mit der ID:%d zurück.", categoryId));
         return new ResponseEntity<Category>(serviceFacade.getCategory(categoryId), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/category/{categoryId}", method = RequestMethod.DELETE)
     public ResponseEntity<Boolean> deleteCategory(@RequestHeader("usr") String username, @RequestHeader("pass") String password, @PathVariable(value = "categoryId") int categoryId) {
-        if(!serviceFacade.checkPermission(new Credential(username, password)))
-            return new ResponseEntity<Boolean>(Boolean.FALSE, HttpStatus.FORBIDDEN);
+        logger.info("Lösche Kategorie aus dem System.");
+        if(!serviceFacade.checkPermission(new Credential(username, password))){
+            logger.info("Keine gültigen Benutzerrechte liegen vor.");
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Boolean>(serviceFacade.deleteCategory(categoryId), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/product", method = RequestMethod.POST)
     public ResponseEntity<Product> createProduct(@RequestHeader("usr") String username, @RequestHeader("pass") String password, @RequestBody Product product) {
-        if(!serviceFacade.checkPermission(new Credential(username, password)))
+        logger.info("Erzeuge ein neues Produkt.");
+        if(!serviceFacade.checkPermission(new Credential(username, password))){
+            logger.info("Keine gültigen Benutzerrechte liegen vor.");
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Product>(serviceFacade.createProduct(product), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/product/{productId}", method = RequestMethod.PUT)
     public ResponseEntity<Product> updateProduct(@RequestHeader("usr") String username, @RequestHeader("pass") String password, @PathVariable(value = "productId") int productId, @RequestBody Product product) {
-        if(!serviceFacade.checkPermission(new Credential(username, password)))
+        logger.info("Aktualisiere ein vorhandenes Produkt.");
+        if(!serviceFacade.checkPermission(new Credential(username, password))){
+            logger.info("Keine gültigen Benutzerrechte liegen vor.");
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Product>(serviceFacade.updateProduct(productId, product), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/product/{productId}", method = RequestMethod.DELETE)
     public ResponseEntity<Boolean> deleteProduct(@RequestHeader("usr") String username, @RequestHeader("pass") String password, @PathVariable(value = "productId") int productId) {
-        if(!serviceFacade.checkPermission(new Credential(username, password)))
+        logger.info("Lösche ein vorhandenes Produkt aus dem System.");
+        if(!serviceFacade.checkPermission(new Credential(username, password))){
+            logger.info("Keine gültigen Benutzerrechte liegen vor.");
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Boolean>(serviceFacade.deleteProduct(productId), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/product", method = RequestMethod.GET)
     public ResponseEntity<List<Product>> getProducts(@RequestParam(name = "categoryId", required = false, defaultValue = "-1") int categoryId) {
+        logger.info("Liefere alle Produkte zurück mit der gegebenen Id.");
         return new ResponseEntity<List<Product>>(serviceFacade.getProducts(categoryId), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/product/{productId}", method = RequestMethod.GET)
     public ResponseEntity<Product> getProduct(@PathVariable(value = "productId") int id) {
+        logger.info("Liefere ein Produkt mit der gegebenen Id zurück.");
         return new ResponseEntity<Product>(serviceFacade.getProduct(id), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/catalog/", method = RequestMethod.GET)
     public ResponseEntity<List<Catalog>> getCatalog() {
+        logger.info("Liefere einen Produktkatalog zurück.");
         return new ResponseEntity<List<Catalog>>(serviceFacade.getCatalog(), HttpStatus.OK);
     }
 
     @Override
     @RequestMapping(value = "/catalog/{categoryId}", method = RequestMethod.GET)
     public ResponseEntity<List<Catalog>> getCatalogCategorie(@PathVariable(value = "categoryId") int categoryId) {
+        logger.info("Liefere ein speziellen Eintrag des Produktkatalogs zurück.");
         return new ResponseEntity<List<Catalog>>(serviceFacade.getCatalogCategorie(categoryId), HttpStatus.OK);
     }
 
@@ -116,6 +140,7 @@ public class CatalogController implements ICatalogController {
     public ResponseEntity<List<Product>> searchCatalog(@RequestParam(name = "minPrice", required = false) Double minPrice,
                                                        @RequestParam(name = "maxPrice", required = false) Double maxPrice,
                                                        @RequestParam(name = "details", required = false, defaultValue = "") String details) {
+        logger.info("Führe eine Suche anhand der gegebenen Parameter zurück.");
         double searchMinPrice = minPrice instanceof Double? minPrice : SEARCH_MIN_PRICE_DEFAULT;
         double searchMaxPrice = maxPrice instanceof Double? maxPrice : SEARCH_MAX_PRICE_DEFAULT;
         return new ResponseEntity<List<Product>>(serviceFacade.searchCatalog(searchMinPrice, searchMaxPrice, details), HttpStatus.OK);
