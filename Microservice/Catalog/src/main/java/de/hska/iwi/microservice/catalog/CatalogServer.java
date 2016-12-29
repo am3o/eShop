a0fab31a2b3c7674630c0c455fb4de17ba000b2c
Joshua Braun <joshuabbraun@gmail.com>
2016-12-29 15:33:44 +0100

    Optimiere Imports bei CatalogServer

--- Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/CatalogServer.java
+++ Microservice/Catalog/src/main/java/de/hska/iwi/microservice/catalog/CatalogServer.java
@@ -5,13 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by ameo on 13.11.16.
