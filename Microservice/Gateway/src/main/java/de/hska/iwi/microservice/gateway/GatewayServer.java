fc938a8f1f0cb9df6d43f7f0238c966b16ca5ee0
Joshua Braun <joshuabbraun@gmail.com>
2016-12-30 13:39:23 +0100

    Gateway bindet dynamisch Routen zu Endpunkten über den Eureka

--- Microservice/Gateway/src/main/java/de/hska/iwi/microservice/gateway/GatewayServer.java
+++ Microservice/Gateway/src/main/java/de/hska/iwi/microservice/gateway/GatewayServer.java
@@ -2,12 +2,14 @@ package de.hska.iwi.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by ameo on 18.11.16.
  */
 @EnableZuulProxy
+@EnableHystrix
 @SpringBootApplication
 public class GatewayServer {
     public static void main(String[] args) {
