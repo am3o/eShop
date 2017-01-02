a4f670b49bfa7c8c646f9e968fe054cd9ccd7415
Joshua Braun <joshuabbraun@gmail.com>
2017-01-02 12:33:57 +0100

    Docker-Container Datenbank wird automatisch mit Inhalt initialisiert

--- Resources/sql/initial.sql
+++ Resources/sql/initial.sql
@@ -1,5 +1,16 @@
-insert into `role` (`id`, `level1`, `type`) values(1, 0, 'admin')
-insert into `role` (`id`, `level1`, `type`) values(2, 1, 'user')
+INSERT INTO `role` (`id`, `level1`, `type`) VALUES (1, 0, 'admin');
+INSERT INTO `role` (`id`, `level1`, `type`) VALUES (2, 1, 'user');
 
 
-insert into `customer` (`id`, `name`, `lastname`, `password`, `username`, `role`) values(1, 'admin', 'admin', 'admin', 'admin', 1)
+INSERT INTO `customer` (`id`, `name`, `lastname`, `password`, `username`, `role`)
+VALUES (1, 'Guenther', 'Kastenbrot', 'sicher', 'admin', 1);
+
+INSERT INTO `product` (`details`, `name`, `price`, `category_id`)
+VALUES ('Das unglaubliche Leben des Torwaechters Zuul', 'Biographie des Zuul', '7.99', '1');
+
+INSERT INTO `category` (`name`) VALUES ('Biographie');
+
+INSERT INTO `product` (`details`, `name`, `price`, `category_id`)
+VALUES ('Es handelt sich um die Grundlagen von REST', 'REST for Dummys', '9.99', '2');
+
+INSERT INTO `category` (`name`) VALUES ('Fachbuecher');
