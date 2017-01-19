c614f4d620c15d743893c323e393a168d30e57e6
Joshua Braun <joshuabbraun@gmail.com>
2017-01-19 15:11:58 +0100

    LogOutAction angepasst an die API

--- WebShopStart/src/main/java/hska/iwi/eShopMaster/controller/LogoutAction.java
+++ WebShopStart/src/main/java/hska/iwi/eShopMaster/controller/LogoutAction.java
@@ -2,20 +2,31 @@ package hska.iwi.eShopMaster.controller;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import hska.iwi.eShopMaster.model.businessLogic.manager.UserManager;
+import hska.iwi.eShopMaster.model.businessLogic.manager.entity.User;
+import hska.iwi.eShopMaster.model.businessLogic.manager.impl.UserManagerImpl;
+import java.util.Map;
 
 public class LogoutAction extends ActionSupport {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -530488065543708898L;
+  /**
+   *
+   */
+  private static final long serialVersionUID = -530488065543708898L;
 
-	public String execute() throws Exception {
+  public String execute() throws Exception {
 
-		// Clear session:
-		ActionContext.getContext().getSession().clear();
-		
-		return "success";
-		
-	}
+    // Clear session:
+    Map<String, Object> session = ActionContext.getContext().getSession();
+
+    UserManager userManager = new UserManagerImpl();
+    User currentUser = (User) session.get("webshop_user");
+
+    userManager.logoutUser(currentUser.getUsername(), currentUser.getPassword());
+
+    session.clear();
+
+    return "success";
+
+  }
 }
