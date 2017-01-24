a1a042084d95835ac26ac2f78deba3db66b82a53
Joshua Braun <joshuabbraun@gmail.com>
2017-01-24 14:53:42 +0100

    Gateway Route zum OAuth hinzugefügt und Mechanismus für OAuth hinterlegt

---
+++ Microservice/Gateway/src/main/java/de/hska/iwi/microservice/gateway/security/DynamicOauth2ClientContextFilter.java
@@ -0,0 +1,48 @@
+package de.hska.iwi.microservice.gateway.security;
+
+import java.io.IOException;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * Created by ameo on 22.01.17.
+ */
+public class DynamicOauth2ClientContextFilter extends OAuth2ClientContextFilter {
+
+  private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+  @Override
+  protected void redirectUser(UserRedirectRequiredException e, HttpServletRequest request,
+      HttpServletResponse response) throws IOException {
+    String redirectUri = e.getRedirectUri();
+    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(redirectUri);
+    Map<String, String> requestParams = e.getRequestParams();
+    for (Map.Entry<String, String> param : requestParams.entrySet()) {
+      builder.queryParam(param.getKey(), param.getValue());
+    }
+
+    if (e.getStateKey() != null) {
+      builder.queryParam("state", e.getStateKey());
+    }
+
+    String url = getBaseUrl(request) + builder.build().encode().toUriString();
+    this.redirectStrategy.sendRedirect(request, response, url);
+  }
+
+  @Override
+  public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+    this.redirectStrategy = redirectStrategy;
+  }
+
+  private String getBaseUrl(HttpServletRequest request) {
+    StringBuffer url = request.getRequestURL();
+    return url.substring(0,
+        url.length() - request.getRequestURI().length() + request.getContextPath().length());
+  }
+}
